b4813bb36864ebbf61494d2da2d57927e3a3cdbb
Aasemaaneh <aasemaaneh8@gmail.com>
2023-06-14 23:56:29 -0400

    stdin setup

--- client.js
+++ client.js
@@ -10,11 +10,42 @@ conn.setEncoding("utf8");
 conn.on("connect", () => {   // your conn object is an instance of the Socket class
    console.log('Successfully connected to game server') // code that does something when the connection is first established
    conn.write("Name: SAM");
-  });
+   conn.write("Move: up");
+   conn.write("Move: down");
+   conn.write("Move: left");
+   conn.write("Move: right");
+});
+conn.on("connect", () => {
+  console.log('Successfully connected to game server');
+
+  setTimeout(() => {
+    conn.write("Move: up");
+  }, 500);
+  
+  setTimeout(() => {
+    conn.write("Move: down");
+  }, 1000);
+  
+  setTimeout(() => {
+    conn.write("Move: left");
+  }, 1500);
+  
+  setTimeout(() => {
+    conn.write("Move: right");
+  }, 2000);
+});
 conn.on("data", (message) => {  
    console.log(message);
 
 });
+
+//conn.on("connect", () => {
+//  console.log('Successfully connected to game server');
+//
+//  setInterval(() => {
+//    conn.write("Move: left");
+//  }, 5);
+//});
 
 return conn; //The conn object that Node returned to you represents the connection that you have with the server.
 //The conn object is full of useful methods and properties that can now be used to interact with the server!
